c32d5f674e594541b65b32285ae95b5ca750dd13
nour eldin mahmoud <https://github.com/Nour5Eldin>
2024-10-20 11:47:57 +0300

    create list o app issues list in Github Repo Ui model

--- app/src/main/java/com/noureldin/githubrepositoryodcamit/presentation/screens/repo_isuues_screen/preview_data/IssuesUiModelPreviewData.kt
+++ app/src/main/java/com/noureldin/githubrepositoryodcamit/presentation/screens/repo_isuues_screen/preview_data/IssuesUiModelPreviewData.kt
@@ -3,10 +3,26 @@ package com.noureldin.githubrepositoryodcamit.presentation.screens.repo_isuues_s
 import com.noureldin.githubrepositoryodcamit.domain.model.IssueState
 import com.noureldin.githubrepositoryodcamit.presentation.screens.repo_isuues_screen.model.IssuesUiModel
 
-val issuesUiModelPreviewData = IssuesUiModel(
-    id = 12345,
-    title = "Issue Title",
-    author = "Seif",
-    date = "Created At: 2023-01-01, 14:00 PM",
-    state = IssueState.Open
+val issuesUiModelPreviewData = listOf(
+    IssuesUiModel(
+        id = 53345,
+        title = "Issues Fixing DataStore in Repo App",
+        author = "Nour",
+        date = "Created At: 2024-10-20, 14:00 PM",
+        state = IssueState.Closed
+    ),
+    IssuesUiModel(
+        id = 12345,
+        title = "Issues in domain Module connecting to main project",
+        author = "Nour",
+        date = "Created At: 2024-9-15, 14:00 PM",
+        state = IssueState.Open
+    ),
+    IssuesUiModel(
+        id = 53345,
+        title = "Issues Provide Issues List in Github",
+        author = "Nour",
+        date = "Created At: 2024-10-20, 14:00 PM",
+        state = IssueState.Closed
+    ),
 )
